9394c51beaa1087886aa9009fca79626901c3762
Bilal Saeed <bilalsaeed232@gmail.com>
2019-02-03 02:58:39 +0100

    ADDED: database connection

--- chain_gang/private/initialize.php
+++ chain_gang/private/initialize.php
@@ -25,6 +25,7 @@
 
   require_once('functions.php');
   require_once('db_credentials.php');
+  require_once('database_functions.php');
   // Load class definitions manually
 
   // -> Individually
@@ -42,5 +43,7 @@
     }
   }
   spl_autoload_register('my_autoload');
+
+  $database = db_connect();
 
 ?>
